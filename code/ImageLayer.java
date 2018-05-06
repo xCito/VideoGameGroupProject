4b35a8b4bfc2291b02b28564f8c1da8f8a627bd9
Charlie Vargas <cvargas9717@hotmail.com>
2018-05-06 01:40:04 -0400

    Add files via upload

--- code/ImageLayer.java
+++ code/ImageLayer.java
@@ -1,11 +1,12 @@
 package code;
 
 import java.awt.*;
+import java.util.LinkedList;
 
 
-public class ImageLayer  {
+public class ImageLayer {
 	
-	//List list = new List();
+	LinkedList<Integer> list = new LinkedList<Integer>() ;
 	
 	Image image;
 	
@@ -13,11 +14,28 @@ public class ImageLayer  {
 	int y;
 	int z;
 	
+	public void LinkedList() {
+		
+		
+		list.add(-500);
+		list.add(-250);
+		list.add(0);
+		list.add(250);
+		list.add(500);
+		
+		
+		
+	}
+	
+	
+	
 	public ImageLayer(String file, int x,int y,int z) {
 		image = Toolkit.getDefaultToolkit().getImage(file);
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		
+		LinkedList();
 	}
 	
 	public void moveLeftBy(int dx) {
@@ -29,7 +47,17 @@ public class ImageLayer  {
 	}
 	
 	public void moveUpBy(int dy) {
-		y+=dy;
+		//y+=dy;
+		for(int i=0; i<list.size(); ++i)
+			list.set(i,list.get(i)+dy);
+		
+		if(list.peekLast() > 768) {
+			list.removeLast();
+			int first = list.peekFirst();
+			
+			list.addFirst(first-250);
+		}
+			
 	}
 	public void moveDownBy(int dy) {
 		y-=dy;
@@ -41,8 +69,8 @@ public class ImageLayer  {
 	
 	public void draw(Graphics g) {
 		//int count = 0;
-		for(int i = 0;i<10;i++) {
-			g.drawImage(image,x,y+(i*250),250,250,null);
+		for(int i = 0;i<list.size();i++) {
+			g.drawImage(image,x,list.get(i),250,250,null);
 			//g.drawImage(image,x,y+350,250,250,null);
 		}
 	}
